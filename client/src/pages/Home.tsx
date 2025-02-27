c69f8dfc172db29a645ee60cd67ed8bc1ec520dd
Colby Carter <cjcarter1223@yahoo.com>
2025-02-26 18:04:21 -0700

    adds react + express + docker starter framework

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+export const Home = () => {
+
+    useEffect(() => {
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
