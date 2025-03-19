e73ccf8f95d291f05d6a04220039a434c2d58d45
Dylan Green <cobaltgoldcsprinks@gmail.com>
2025-03-19 11:47:52 -0600

    Update Sidebar CSS to enable "prettier" extension
    This should make the sidebar a little nicer to look at in the visualMap
    component

--- client/src/pages/VisualMap.tsx
+++ client/src/pages/VisualMap.tsx
@@ -8,14 +8,14 @@ import { useState } from "react";
 export const VisualMap: () => JSX.Element = () => {
     let [selectedMode, setSelectedMode] = useState<MapViewType>(MapViewType.STOPS);
 
-    let markers: JSX.Element[];
+    let markers: JSX.Element[] = [];
     if (selectedMode.valueOf() === MapViewType.STOPS) {
         markers = StopMarkers();
     }
     return (
         <div id="super-container">
         <div className="layout-container">
-        <MapContainer center={[41, -112]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[41, -112]} zoom={6} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
